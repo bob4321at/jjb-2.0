4e0b027e41bb0560913a6551f905717fc43303cf
bob4321at <judeberin113@gmail.com>
2025-03-13 17:24:47 -0600

    gave gojo his moveset back

--- players/gojo.go
+++ players/gojo.go
@@ -13,7 +13,36 @@ func (player *Player) gojoRed() {
 }
 
 func (player *Player) gojoBlue() {
-	player.NewProjectile(utils.Vec2{X: player.Pos.X, Y: player.Pos.Y}, utils.Vec2{X: player.Pos.X + player.Vel.X - utils.Mouse_X - camera.Cam.Offset.X + 640 + (float64(player.Img.GetTexture().Bounds().Dx())), Y: player.Pos.Y + player.Vel.Y - utils.Mouse_Y - camera.Cam.Offset.Y + 320 + (float64(player.Img.GetTexture().Bounds().Dy()))}, 1, 10, 5, 5, textures.NewTexture("./art/projectiles/gojo/blue.png"))
+	player.NewEntity(player.Pos, utils.Vec2{}, 0, 1, textures.NewTexture("./art/entities/gojo/blue.png"), gojoBlueAi)
+}
+
+func gojoBlueAi(entity *PlayerEntity, level_hitbox []utils.HitBox) {
+	entity.Lifespan -= 0.1
+
+	entity.Pos.X = Player_Ref.Pos.X + ((utils.Mouse_X / 1.5) - (1280 - (1280 / 1.5))) - 72
+	entity.Pos.Y = Player_Ref.Pos.Y + ((utils.Mouse_Y / 1.5) - (720 - (720 / 1.5))) - 72
+	for ei := 0; ei < len(enemyai.Enemies_In_World); ei++ {
+		enemy := enemyai.Enemies_In_World[ei]
+
+		if utils.Collide(utils.Vec2{X: entity.Pos.X - 144, Y: entity.Pos.Y}, utils.Vec2{X: 144 * 3, Y: 144 * 3}, enemy.Pos, utils.Vec2{X: float64(enemy.Tex.GetTexture().Bounds().Dx()), Y: float64(enemy.Tex.GetTexture().Bounds().Dy())}) {
+
+			enemy.Can_Move = false
+			if enemy.Pos.X >= entity.Pos.X+72 {
+				enemy.Vel.X = -0.5
+			} else {
+				enemy.Vel.X = 0.5
+			}
+			if enemy.Pos.Y >= entity.Pos.Y+72 {
+				enemy.Vel.Y = -0.5
+			} else {
+				enemy.Vel.Y = 0.5
+			}
+			enemy.Pos.X += enemy.Vel.X
+			enemy.Pos.Y += enemy.Vel.Y
+			enemy.Can_Move = true
+		}
+	}
+	// extra math is to convert mouse_cords to world cords
 }
 
 func (player *Player) gojoPurple() {
@@ -45,6 +74,7 @@ func (player *Player) gojoDomain(enemies []*enemyai.Enemy) {
 	for start_time+1500 > utils.Game_Time {
 		for enemy_index := 0; enemy_index < len(affected); enemy_index++ {
 			affected_enemy := affected[enemy_index]
+			affected_enemy.enemy.Can_Move = false
 			if affected_enemy.enemy.Health < 0 {
 				affected_enemy.alive = false
 			}
@@ -61,7 +91,7 @@ func (player *Player) gojoDomain(enemies []*enemyai.Enemy) {
 }
 
 var gojo_attacks = []Attack{
-	{Player_Ref.gojoBlue, 0, 3},
-	{Player_Ref.gojoRed, 0, 5},
+	{Player_Ref.gojoRed, 0, 3},
+	{Player_Ref.gojoBlue, 0, 0.1},
 	{Player_Ref.gojoPurple, 0, 20},
 }
